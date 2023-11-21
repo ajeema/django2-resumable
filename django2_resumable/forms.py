9d8d25f30dd13478759b11851c24c20b98b91676
Aaron Jorgensen <62412972+ajeema@users.noreply.github.com>
2023-11-21 08:44:17 -0600

    Update forms.py

--- django2_resumable/forms.py
+++ django2_resumable/forms.py
@@ -1,11 +1,12 @@
 from django.core.exceptions import ValidationError
 from django.forms import FileField
-
 from django2_resumable.widgets import ResumableWidget
-
 
 class FormResumableFileField(FileField):
     widget = ResumableWidget
+    default_error_messages = {
+        'empty': "This field cannot be empty.",  # Customize this message as needed
+    }
 
     def to_python(self, data):
         if self.required:
